dbd16d00838aeafe67538315c6f9aab78cb16f60
alexfar88 <alibabba101@gmail.com>
2024-06-15 14:48:48 -0400

    oops

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let slideIndex = 0;
 let rotations = [];
 const images = [
-  'The first date!!',
+  'images/Business-images-0.jpg',
   'images/Business-images-1.jpg',
   'images/Business-images-2.jpg',
   'images/Business-images-3.jpg',
